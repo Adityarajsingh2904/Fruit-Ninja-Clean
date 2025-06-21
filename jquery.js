521ce5e6dc29f060165e44315f111ccb27f4a6d5
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-21 09:44:03 +0530

    Resolve merge conflicts and integrate features

--- jquery.js
+++ jquery.js
@@ -7,9 +7,9 @@ var highScore=0;
 
 var fruits = ['apple', 'banana', 'grapes', 'mango', 'orange', 'peach', 'pear', 'pineapple','tomato','watermelon'];
 
-function getHighScore() {
+function getHighScore(){
     const stored = localStorage.getItem('highScore');
-    if(stored !== null && !isNaN(parseInt(stored))) {
+    if(stored !== null && !isNaN(parseInt(stored))){
         highScore = parseInt(stored);
     } else {
         highScore = 0;
@@ -21,32 +21,62 @@ function updateHighScoreDisplay(){
     $("#hsValueGameOver").text(highScore);
 }
 
+function startGame(){
+    isPlaying=true;
+    score=0;
+    dropSpeed=1;
+    $("#value").html(score);
+    $("#menubar").hide();
+    $("#endgame").hide();
+    $("#liferem").css("display","flex");
+    $("#container").css({"display":"flex"});
+    lives=3;
+    addHeart();
+    startFruits();
+}
+
 $(function(){
     getHighScore();
     updateHighScoreDisplay();
+
     $("#start").click(function(){
-        if(isPlaying==true){
+        if(isPlaying){
             location.reload();
+        } else {
+            startGame();
         }
-        else{
-            isPlaying=true;
-            score=0;
-            $("#value").html(score);
-            $("#menubar").hide();
-            $("#liferem").css('display','flex');
-            $("#container").css({'display':'flex'});
-            lives=3;
-            addHeart();
-            startFruits();
-        }
+    });
+
+    $("#restartGame").click(function(){
+        startGame();
     });
 
     $("#restart").click(function(){
         location.reload();
     });
+
+    $("#toggleDark").click(function(){
+        $("body").toggleClass("dark");
+    });
 });
 
-$("#fruit").mouseover(cut);
+$("#fruit").on("mouseover touchstart", cut);
+
+$("#container").on("mousemove touchmove", function(event){
+    const x = event.pageX || event.touches?.[0]?.pageX;
+    const y = event.pageY || event.touches?.[0]?.pageY;
+    const fruit = $("#fruit");
+
+    if(fruit.is(":visible")){
+        const offset = fruit.offset();
+        const w = fruit.width();
+        const h = fruit.height();
+        if(x >= offset.left && x <= offset.left + w &&
+           y >= offset.top && y <= offset.top + h){
+            cut();
+        }
+    }
+});
 
 function addHeart(){
     $("#life").empty();
@@ -63,32 +93,25 @@ function startFruits(){
         dropSpeed+=1;
     }
     action = setInterval(function(){
-
         $("#fruit").css('top', $("#fruit").position().top + dropSpeed);
         if($("#fruit").position().top > $("#container").height()){
-
             if(lives > 1 ){
-
                 $("#fruit").css({'display':'flex'});
                 chooseFruit();
                 $("#fruit").css({'left' : Math.round(($("#container").width()-350)*Math.random())+200, 'top' : -50});
-
                 lives-=1;
-
                 addHeart();
-
             }else{
                 isPlaying = false;
                 $("#liferem").css('display','none');
                 $("#fsc").text(score);
                 lives-=1;
-
                 addHeart();
                 $("#endgame").show();
                 stopAction();
             }
         }
-    },10)
+    },10);
 }
 
 function chooseFruit(){
@@ -115,3 +138,4 @@ function cut(){
 
     setTimeout(startFruits, 800);
 }
+
