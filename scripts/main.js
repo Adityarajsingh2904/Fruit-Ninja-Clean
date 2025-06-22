4efd0b256d7d0f3973034c95d19316a61ee78138
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-23 05:10:53 +0530

    Remove placeholder script

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-// Placeholder for extracted inline scripts
-
-// Basic game logic initialization
-window.onload = function () {
-  console.log("Game initialized");
-  // TODO: Setup canvas, fruit spawning, collision detection
-};
