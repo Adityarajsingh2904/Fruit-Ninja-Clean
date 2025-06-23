8cd8451b64f2b8ab1b6abe1bbd49e0b237a7fbce
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-06-23 05:35:54 +0530

    Fix fruit image assignment

--- scripts/main.js
+++ scripts/main.js
@@ -51,7 +51,9 @@ window.onload = function () {
 
   function spawnFruit() {
     const img = new Image();
-    img.src = "images/" + fruitImages[Math.floor(Math.random() * fruitImages.length)];
+    // randomly pick an image for the fruit
+    img.src =
+      "images/" + fruitImages[Math.floor(Math.random() * fruitImages.length)];
     const radius = 30;
     const x = Math.random() * (canvas.width - radius * 2) + radius;
     const y = canvas.height + radius;
